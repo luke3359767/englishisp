ff1470cdc63a60552ea9e00425c7a52295302bcf
luke3359767 <3359767@gmail.com>
2021-10-08 02:35:54 +0000

    n

--- literacy-analyze/src/components/App.js
+++ literacy-analyze/src/components/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
+// import ReactDOM  from 'react-dom';
+// eslint-disable-next-line
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './Header';
+
 
 function App() {
   return (
     <dev>
       <Header/>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/Vlog" component="Vlog"></Route>
+          <Route path="/Blog" component="Blog"></Route>
+        </Switch>
+      </BrowserRouter>
     </dev>
   );
 }
---
+++ literacy-analyze/src/components/Vlog.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Vlog=()=>{
+    return(
+        <div>
+            <h1>VLOG</h1>
+            <h1>VLOG</h1>
+            <h1>VLOG</h1>
+            <h1>VLOG</h1>
+            <h1>VLOG</h1>
+            <h1>VLOG</h1>
+        </div>
+    );
+}
+
+export default Vlog;
