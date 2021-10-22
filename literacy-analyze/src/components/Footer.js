39e2aa61ec852d44c227b30c09eb900ee0d76f64
luke3359767 <3359767@gmail.com>
2021-10-22 17:50:05 +0000

    commit

--- literacy-analyze/src/components/Footer.js
+++ literacy-analyze/src/components/Footer.js
@@ -2,13 +2,19 @@ import React from "react";
 
 const style={
     background:"#38318f",
-    
+    paddingBottom:"1px",
+    color:"#fff",
+}
+
+const pStyle={
+    paddingTop:"6px",
+    marginLeft:"30px"
 }
 
 const Footer=()=>{
     return(
         <div style={style}>
-            <p>Footer</p>
+            <p style={pStyle}>©2020 by Luke Liu </p>
         </div>
     );
 }
