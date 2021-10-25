f3646cc8f82ccaa833b21b2bb39c2a700675f74b
luke3359767 <3359767@gmail.com>
2021-10-25 03:20:52 +0000

    commit

--- literacy-analyze/src/components/Vlog.js
+++ literacy-analyze/src/components/Vlog.js
@@ -1,20 +1,31 @@
 import React from "react";
+import ReactPlayer from 'react-player/youtube'
 
 const style={
     height: '460px',
     overflow: "scroll",
+    alignItems: "center",
 
 }
 
 const vstyle={
     height:"400px",
+    marginLeft:"auto",
+    marginRight:"auto",
 }
 
 const Vlog=()=>{
     return(
         <div style={style}>
             <h1 style={{paddingTop:"20px"}}>VLOG</h1>
-            <video style={vstyle} src={`http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4`}/>
+            <ReactPlayer
+                url="https://youtu.be/luai0p0y2zE"
+                controls
+                playbackRate = {2}
+                height = "350px"
+                width="750px"
+                style={vstyle}
+            />
             
         </div>
     );
